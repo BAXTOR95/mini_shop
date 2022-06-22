9281ad5a153ff101a0470fa6e44d083be5e66a92
Brian Arriaga <brian.arriaga@gmail.com>
2022-06-21 20:56:45 -0400

    Added first project files

---
+++ frontend/src/containers/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../hocs/Layout';
+
+const checkout = () => (
+    <Layout
+        title='Checkout'
+        content=''
+    >
+        <h1>Checkout Page</h1>
+    </Layout>
+)
+
+export default checkout;
---
+++ frontend/src/containers/ThankYou.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../hocs/Layout';
+
+const thankyou = () => (
+    <Layout
+        title='Thank You'
+        content=''
+    >
+        <h1>Thank You Page</h1>
+    </Layout>
+)
+
+export default thankyou;
